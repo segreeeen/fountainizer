c69008f61fe5de54ad5cc70ca734ec38aab66d15
Thomas Sulzbacher <thomas110793@gmail.com>
2016-01-01 23:33:38 +0100

    removed unused imports. got sick of warnings xD

--- src/at/hacksolutions/f2p/blaaaa.java
+++ src/at/hacksolutions/f2p/blaaaa.java
@@ -1,17 +1,6 @@
 package at.hacksolutions.f2p;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import at.hacksolutions.f2p.io.FileReader;
-import at.hacksolutions.f2p.parser.Parser;
-import at.hacksolutions.f2p.parser.line.DynamicLines;
-import at.hacksolutions.f2p.parser.line.Formatter;
-import at.hacksolutions.f2p.parser.line.SimpleLine;
-import at.hacksolutions.f2p.parser.types.ParserConstants;
-import at.hacksolutions.f2p.parser.types.TitleLineType;
-import at.hacksolutions.f2p.parser.types.TypeHelper;
 
 public class blaaaa {
     public static void main(String[] args) throws IOException {
