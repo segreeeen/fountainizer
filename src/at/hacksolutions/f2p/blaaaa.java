3d6dd2e5299f7d44657b2644f93959f01f44249e
Felix <felix@batusic.net>
2016-01-03 20:31:02 +0100

    nothing to see here, move along....

--- src/at/hacksolutions/f2p/blaaaa.java
+++
@@ -1,9 +0,0 @@
-package at.hacksolutions.f2p;
-
-import java.io.IOException;
-
-public class blaaaa {
-    public static void main(String[] args) throws IOException {
-	System.out.println("    BRICK & STEEL".trim());
-    }
-}
