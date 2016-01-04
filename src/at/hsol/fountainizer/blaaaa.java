d00b4534995e950fad56863e9bb01e83e8ec15f8
Felix <felix@batusic.net>
2016-01-04 21:48:43 +0100

    moving some things

--- src/at/hsol/fountainizer/blaaaa.java
+++
@@ -1,9 +0,0 @@
-package at.hsol.fountainizer;
-
-import java.io.IOException;
-
-public class blaaaa {
-    public static void main(String[] args) throws IOException {
-	System.out.println("    BRICK & STEEL".trim());
-    }
-}
