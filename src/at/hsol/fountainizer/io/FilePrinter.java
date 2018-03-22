71991b0968ee7072cf8769fd76773a072269ade8
Felix <felix@batusic.net>
2018-03-22 21:52:09 +0100

    missed one

--- src/at/hsol/fountainizer/io/FilePrinter.java
+++ src/at/hsol/fountainizer/io/FilePrinter.java
@@ -2,6 +2,7 @@ package at.hsol.fountainizer.io;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+
 import at.hsol.fountainizer.Options;
 import at.hsol.fountainizer.parser.content.ParserContent;
 import at.hsol.fountainizer.parser.meta.Statistic;
@@ -15,24 +16,26 @@ import at.hsol.fountainizer.pdfbox.pager.TitlePager;
  * @author Felix Batusic
  */
 public class FilePrinter {
-    
-    Options options;
-    
-    public FilePrinter(Options options) {
-	this.options = options;
-    }
-    
-    public void writePDFBox(ParserContent dLines, String fileName, Statistic stats) throws IOException, URISyntaxException {
-	PagerController controller = new PagerController(80, 40, 60, 80, options);
-	StandardPager pager = controller.getPager(PagerController.PagerType.STANDARD_PAGER);
-	//CustomScriptPager custompager = controller.getPager(PagerController.PagerType.CUSTOM_PAGER);
-	TitlePager titlePager = controller.getPager(PagerController.PagerType.TITLE_PAGER);
-	CharacterPager cPager = controller.getPager(PagerController.PagerType.CHARACTER_PAGER);
-	
-	titlePager.printContent(dLines.getTitlepage());
-	cPager.printContent(stats.getCharacters().getCharacters());
-	pager.printContent(dLines);
-	
-	controller.finalizeDocument(fileName);
-    }
+
+	Options options;
+
+	public FilePrinter(Options options) {
+		this.options = options;
+	}
+
+	public void writePDFBox(ParserContent dLines, String fileName, Statistic stats)
+			throws IOException, URISyntaxException {
+		PagerController controller = new PagerController(80, 40, 60, 80, options);
+		StandardPager pager = controller.getPager(PagerController.PagerType.STANDARD_PAGER);
+		// CustomScriptPager custompager =
+		// controller.getPager(PagerController.PagerType.CUSTOM_PAGER);
+		TitlePager titlePager = controller.getPager(PagerController.PagerType.TITLE_PAGER);
+		CharacterPager cPager = controller.getPager(PagerController.PagerType.CHARACTER_PAGER);
+
+		titlePager.printContent(dLines.getTitlepage());
+		cPager.printContent(stats.getCharacters().getCharacters());
+		pager.printContent(dLines);
+
+		controller.finalizeDocument(fileName);
+	}
 }
